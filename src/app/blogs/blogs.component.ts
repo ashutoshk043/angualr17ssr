59af74092f7e08cc8710d8cbacc51c06bf77125a
@ashutosh kum@r <ashutoshitm043@gmail.com>
2024-04-12 12:43:02 +0530

    added api calls

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
@@ -11,9 +12,17 @@ export class BlogsComponent implements OnInit {
   data:any = "<p>यह एक लंबा स्थापित तथ्य है कि जब एक पाठक एक पृष्ठ के खाखे को देखेगा तो पठनीय सामग्री से विचलित हो जाएगा. Lorem Ipsum का उपयोग करने का मुद्दा यह है कि इसमें एक और अधिक या कम अक्षरों का सामान्य वितरण किया गया है, 'Content here, content here' प्रयोग करने की जगह इसे पठनीय English के रूप में प्रयोग किया जाये. अब कई डेस्कटॉप प्रकाशन संकुल और वेब</p>";
   blogs!: SafeHtml;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private httpClient:HttpClient) {}
 
   ngOnInit() {
     this.blogs = this.sanitizer.bypassSecurityTrustHtml(this.data);
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe({
+      next: (res: any) => {
+        console.log(res)
+      },
+      error: (error: any) => {
+        console.log(error.message)
+      }
+    })
   }
 }
